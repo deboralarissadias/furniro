d10963b523102acecb44d6c484e6bca4334bd923
Débora Larissa <larissadias.life@gmail.com>
2024-10-25 10:42:15 -0300

    fix: ajustes na página shop e na single product

--- furniro-frontend/src/pages/single-product/single-product.tsx
+++ furniro-frontend/src/pages/single-product/single-product.tsx
@@ -1,19 +1,25 @@
-import React, { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
-import Header from '../../components/header/header';
-import Footer from '../../components/footer/footer';
-import Tabs from '../../components/tabs/tabs';
-import Breadcrumb from '../../components/breadcrumb/breadcrumb';
-import { ProductProps } from '../../interfaces/products';
-import { GET_PRODUCT } from '../../config/endpoints';
-import { productsMock } from '../../components/products/mockData';
+import React, { useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import Header from "../../components/header/header";
+import Footer from "../../components/footer/footer";
+import Tabs from "../../components/tabs/tabs";
+import Breadcrumb from "../../components/breadcrumb/breadcrumb";
+import { ProductProps } from "../../interfaces/products";
+import { GET_PRODUCT, GET_PRODUCTS } from "../../config/endpoints";
+import { productsMock } from "../../components/products/mockData";
+import "./single-product.css"
+import ProductsContainer from "../../components/products/products";
 
 const SingleProduct: React.FC = () => {
+  const navigate = useNavigate();
   const { id } = useParams<{ id: string }>();
 
-  const [product, setProduct] = useState<ProductProps | any>({}); 
-  const [loading, setLoading] = useState(true); 
-
+  const [product, setProduct] = useState<ProductProps | any>({});
+  const [products, setProducts] = useState<ProductProps[]>([]);
+  const [loading, setLoading] = useState(true);
+  // Estado para armazenar a imagem selecionada
+  const [selectedImage, setSelectedImage] = useState(product.image_link);
+  const [quantity, setQuantity] = useState(1);
 
   useEffect(() => {
     // Fazendo a requisição para o endpoint
@@ -23,77 +29,170 @@ const SingleProduct: React.FC = () => {
       try {
         const response = await fetch(url); // Substitua pela URL real do seu endpoint
         const data = await response.json(); // Supondo que a resposta esteja em formato JSON
-        console.log("Produtos recebidos na SHop PAge:", data);
         setProduct(data); // Atualiza o estado com os produtos
         setLoading(false); // Define que o carregamento foi concluído
+        setSelectedImage(data.image_link);
 
-        
       } catch (error) {
         setProduct(productsMock[0]);
-        console.error('Erro ao buscar produto pelo ID:', error);
+        console.error("Erro ao buscar produto pelo ID:", error);
         setLoading(false); // Em caso de erro, para o carregamento
       }
     };
 
+    // Fazendo a requisição para o endpoint
+    const fetchProducts = async () => {
+      try {
+        const categoryId = product?.category_id || 1;
+        const response = await fetch(
+          GET_PRODUCTS + "?page=1&limit=4&categoryId=" + categoryId
+        ); 
+        const dataProducts = await response.json();
+        setProducts(dataProducts.products);
+        setLoading(false);
+      } catch (error) {
+        setProducts(productsMock);
+        console.error("Erro ao buscar produtos:", error);
+        setLoading(false); 
+      }
+    };
+    
+    fetchProducts(); 
     fetchProduct(); // Chama a função ao montar o componente
   }, []); // O array vazio [] garante que o efeito será executado apenas uma vez ao montar o componente
 
-  function convertToSlug(name:string) {
+  function convertToSlug(name: string) {
     if (!name) {
-      return '';
+      return "";
     }
-    
+
     return name
       .toLowerCase() // Converte para minúsculas
       .trim() // Remove espaços em branco do início e do fim
-      .replace(/[^a-z0-9 -]/g, '') // Remove caracteres especiais
-      .replace(/\s+/g, '-') // Substitui espaços por hífens
-      .replace(/-+/g, '-'); // Substitui múltiplos hífens por um único hífen
+      .replace(/[^a-z0-9 -]/g, "") // Remove caracteres especiais
+      .replace(/\s+/g, "-") // Substitui espaços por hífens
+      .replace(/-+/g, "-"); // Substitui múltiplos hífens por um único hífen
   }
 
   const breadcrumbPaths = [
-    { name: 'Home', path: '/' },
-    { name: 'Shop', path: '/shop' },
-    { name: product.name, path: '/product/' + product.id + '/' + convertToSlug(product.name) },
+    { name: "Home", path: "/" },
+    { name: "Shop", path: "/shop" },
+    {
+      name: product.name,
+      path: "/product/" + product.id + "/" + convertToSlug(product.name),
+    },
   ];
+
+  
+
+  // Função para atualizar a imagem principal
+  const handleThumbnailClick = (image: any) => {
+    setSelectedImage(image);
+  };
+
+  const handleShowMore = () => {
+    navigate("/products?categoryId=" + product.category_id);
+  };
 
   return (
     <div>
       <Header />
       <Breadcrumb paths={breadcrumbPaths} isSingleProduct={true} />
 
-      <div>
-        <div>
-          <img src={product.image_link} alt={product.name} className='main-img'/>
-          <h1 className='product-name'>{product.name}</h1>
-          <div className="product-price">{product.price}</div>
-          <div className="product-rating">Rating: {product.rating}</div>
-          <div className="product-description">{product.description}</div>
-          <div>Size</div>
-          <div>Color</div>
-          <select>
-            <option value="1">1</option>
-            <option value="2">2</option>
-            <option value="3">3</option>
-            <option value="4">4</option>
-            <option value="5">5</option>
-          </select>
-          <button className="add-to-cart-btn">Add to Cart</button>
-          <button className="compare-btn">+ Compare</button>
+      <div className="product-details">
+        <div className="product-images">
+          <img
+            src={selectedImage}
+            alt={product.name}
+            className="main-image"
+          />
+          <div className="thumbnail-images">
+            {product?.other_images_link?.map((image: any) => (
+              <img 
+                src={image}
+                alt={product.name}
+                key={image}
+                onClick={() => handleThumbnailClick(image)}
+              />
+            ))}
+          </div>
         </div>
+        <div className="product-info">
+          <h1 className="product-title">Asgaard Sofa</h1>
+          <p className="price">Rs. 2,500,000.00</p>
+          <div className="ratings">
+            <span>⭐⭐⭐⭐⭐</span> <span>(5 Customer Reviews)</span>
+          </div>
+          <p className="description">
+            Setting the bar as one of the loudest speakers in its class, the
+            Kilburn is a compact, stout-hearted hero with a well-balanced audio
+            which boasts a clear midrange and extended highs for a sound.
+          </p>
 
+          <div className="product-options">
+            <div className="size-options">
+              <label>Size: </label>
+              <button>L</button>
+              <button>XL</button>
+              <button>XS</button>
+            </div>
+            <div className="color-options">
+              <label>Color: </label>
+              <button className="color purple"></button>
+              <button className="color black"></button>
+              <button className="color brown"></button>
+            </div>
+          </div>
 
+          <div className="quantity">
+            <button onClick={() => setQuantity(Math.max(1, quantity - 1))}>-</button>
+            <input 
+              type="number" 
+              value={quantity} 
+              onChange={(e) => setQuantity(Number(e.target.value))} // Atualiza o estado com o novo valor
+            />
+            <button onClick={() => setQuantity(quantity + 1)}>+</button>
+          </div>
+
+          <div className="action-buttons">
+            <button className="add-to-cart">Add to Cart</button>
+            <button className="compare">Compare</button>
+          </div>
+
+          <div className="product-meta">
+            <p>SKU: SS001</p>
+            <p>Category: {product.category_id}</p>
+            <p>Tags: Sofa, Chair, Home, Shop</p>
+          </div>
+
+          <div className="share">
+            <p>Share:</p>
+            <a href="#">
+              <i className="fab fa-facebook"></i>
+            </a>
+            <a href="#">
+              <i className="fab fa-twitter"></i>
+            </a>
+            <a href="#">
+              <i className="fab fa-linkedin"></i>
+            </a>
+          </div>
+        </div>
       </div>
-      
-      <div className='description'>
+
+      <div className="description">
         <Tabs />
       </div>
 
+      
+      <div className="single-products">
+        <h1 className="single-products-text">Related Products</h1>
+        <ProductsContainer products={products} pageType="single" />
+        <button className="single-products-button" onClick={handleShowMore}>
+          Show More
+        </button>
+      </div>
 
-
-
-
-      <h1>Product Details for Product ID: {id}</h1>
       <Footer />
     </div>
   );
